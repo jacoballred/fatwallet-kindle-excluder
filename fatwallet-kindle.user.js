b379ea59a21760bac454e22c9947590c21924a74
Jacob Allred <jacob@jacoballred.com>
2014-11-14 08:37:33 -0600

    Add @grant. Hide more books.
    Added an @grant clause to specify that we don't need special permissions. Added another string to search for when excluding Kindle books.

--- fatwallet-kindle.user.js
+++ fatwallet-kindle.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         FatWallet Kindle Excluder
 // @namespace    http://www.jacoballred.com/userscripts/fatwallet-kindle/
-// @version      1.1
+// @version      1.2
 // @description  Exclude Kindle books from the Free Stuff forum listings.
 // @homepage     http://www.jacoballred.com/
 // @updateURL    http://www.jacoballred.com/userscripts/fatwallet-kindle.user.js
 // @downloadURL  http://www.jacoballred.com/userscripts/fatwallet-kindle.user.js
 // @match        http://www.fatwallet.com/forums/free-stuff/
 // @match        http://www.fatwallet.com/forums/hot-deals/
+// @grant        none
 // @copyright    © 2014 Corban Works, LLC
 // ==/UserScript==
 
@@ -18,6 +19,7 @@ $(function() {
     };
     
     $("tr.topicRow:Contains('kindle book'), tr.topicRowHover:Contains('kindle book')").remove();
+    $("tr.topicRow:Contains('kindle edition'), tr.topicRowHover:Contains('kindle edition')").remove();
     $("tr.alt_row").removeClass("alt_row");
     $("tr.topicRow, tr.topicRowHover").filter(":odd").addClass("alt_row");
 });
