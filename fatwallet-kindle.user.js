e71a12014a71bc31075d6e03899d49c4d9c00f9b
Jacob Allred <jacob@jacoballred.com>
2014-11-17 11:16:56 -0600

    More strings to match on

--- fatwallet-kindle.user.js
+++ fatwallet-kindle.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         FatWallet Kindle Excluder
 // @namespace    http://www.jacoballred.com/userscripts/fatwallet-kindle/
-// @version      1.2
+// @version      1.3
 // @description  Exclude Kindle books from the Free Stuff forum listings.
 // @homepage     http://www.jacoballred.com/
 // @updateURL    http://www.jacoballred.com/userscripts/fatwallet-kindle.user.js
@@ -12,14 +12,25 @@
 // @copyright    © 2014 Corban Works, LLC
 // ==/UserScript==
 
-$(function() {  
+$(function() {
     jQuery.expr[':'].Contains = function(a, i, m) {
       return jQuery(a).text().toUpperCase()
           .indexOf(m[3].toUpperCase()) >= 0;
     };
-    
-    $("tr.topicRow:Contains('kindle book'), tr.topicRowHover:Contains('kindle book')").remove();
+
+    // Remove "kindle edition", "(kindle)"
     $("tr.topicRow:Contains('kindle edition'), tr.topicRowHover:Contains('kindle edition')").remove();
+    $("tr.topicRow:Contains('(kindle)'), tr.topicRowHover:Contains('(kindle)')").remove();
+
+    // Remove "kindle book", "kindle ebook", "kindle tech book", "kindle tech ebook"
+    var regex = new RegExp(/kindle (tech )?(e)?book/i);
+    $("tr.topicRow").filter(function () {
+        return regex.test($(this).text());
+    }).remove();
+    $("tr.topicRowHover").filter(function () {
+        return regex.test($(this).text());
+    }).remove();
+
     $("tr.alt_row").removeClass("alt_row");
     $("tr.topicRow, tr.topicRowHover").filter(":odd").addClass("alt_row");
 });
